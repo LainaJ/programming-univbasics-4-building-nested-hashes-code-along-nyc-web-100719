95ed6f32195e6d8ac2948a309e85bc9972573f99
Laina Karosic <lainakarosic@gmail.com>
2019-09-09 20:58:01 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,16 +3,16 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {},
-      :matriarch {},
-      :here {},
-      :hero_friends []
+      :matriarch => {},
+      :here => {},
+      :hero_friends => []
       },
     :capulet => {
       #your key/value pairs here!
-      :patriarch {},
-      :matriarch {},
-      :heroine {},
-      :heroine_friends []
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
 
