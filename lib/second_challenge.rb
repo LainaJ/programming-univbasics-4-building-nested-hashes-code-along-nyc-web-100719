3ef40fb000e77a23a4f4db9aad136ab8fc8f13c4
Laina Karosic <lainakarosic@gmail.com>
2019-09-09 20:50:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       :patriarch {},
       :matriarch {},
       :here {},
-      :hero_friends {}
+      :hero_friends []
       },
     :capulet => {
       #your key/value pairs here!
